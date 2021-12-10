2f7cafc3927ec754dec721f7b2371c11b0e27329
Michel Atieno <mishqamish@gmail.com>
2021-12-10 14:38:25 +0300

    Tiles have registered in svg element

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ let movieDataUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree
 
 let movieData
 
-let canvas = d3.select('canvas')
+let canvas = d3.select('#canvas')
 
 let drawTreeMap = () => {
 
+    let hierarchy = d3.hierarchy(movieData, (node) => {
+        return node['children']
+    }).sum((node) => {
+        return node['value']
+    }).sort((node1, node2) => {
+        return node2['value'] - node1['value']
+    })
+
+    let createTreeMap = d3.treemap()
+                          .size([1000, 600])
+
+    createTreeMap(hierarchy)
+
+    let movieTiles = hierarchy.leaves()
+    console.log(movieTiles)
+
+    let block =  canvas.selectAll('g')
+                        .data(movieTiles)
+                        .enter()
+                        .append('g')
+    
+    block.append('rect')
+         .attr('class', 'tile')
 }
 
 d3.json(movieDataUrl).then(
